318b8819d62dc0a396fae9548e7ec5cb5602ce2f
Zuriel <2001555@sit.singaporetech.edu.sg>
2021-11-29 19:18:47 +0800

    ok

--- codeblockCar/codeblockCar/urls.py
+++ codeblockCar/codeblockCar/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import include, url
+
 # from . import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('codingPage/', include('codingPage.urls')),
-]
+    
+] 
