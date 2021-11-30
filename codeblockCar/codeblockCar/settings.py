56754c1299e7eb8b6ba202c4f6ca91e33aefd4b5
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2021-11-30 17:51:01 +0800

    0.1.3 js script and library fix
    Fixed the src code for js files and added the whole blockly codes

--- codeblockCar/codeblockCar/settings.py
+++ codeblockCar/codeblockCar/settings.py
@@ -118,12 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'templates'),
+    "/var/www/templates"
 ]
+
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
