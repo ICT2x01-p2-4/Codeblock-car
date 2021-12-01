3c455e8d4f8588f16d2c35164881585a3ca7485f
Zuriel <2001555@sit.singaporetech.edu.sg>
2021-12-01 15:38:58 +0800

    stuck

---
+++ codeblockCar/templates/blockly/generators/javascript/custom.js
@@ -0,0 +1,23 @@
+Blockly.JavaScript['forward'] = function (block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return 1;
+};
+
+Blockly.JavaScript['stop'] = function (block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return 2;
+};
+
+Blockly.JavaScript['left'] = function (block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return 3;
+};
+
+Blockly.JavaScript['right'] = function (block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return 4;
+};
