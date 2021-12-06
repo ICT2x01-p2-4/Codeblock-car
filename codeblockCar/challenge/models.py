74ef0f6ebf1e00f2fb098fbc04ad23c4450276b8
ChanIvan98 <72805630+ChanIvan98@users.noreply.github.com>
2021-12-07 01:26:11 +0800

    Revert "Feature connection"

--- codeblockCar/challenge/models.py
+++ codeblockCar/challenge/models.py
@@ -5,7 +5,7 @@ class Challenge(models.Model):
     map = models.TextField()
     size = models.IntegerField()
     difficulty = models.CharField(max_length=15)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
     
     def __str__(self) -> str:
--- codeblockCar/connections/views.py
+++ codeblockCar/connections/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def getInstructions(request):
-    if request.method == "GET":
-        # Retrieve data from db
-        payload = {
-            'commands': '13121'
-        }
-        return render(request, 'test.html',payload)
-
-def getData(request):
-    if request.method == "GET":
-        print(request.GET['dist'])
-        return render(request, 'test.html')
