2c4577b3dc6e57356ec538ea0923850de05244b8
Donovan Cham <28041835+donovancham@users.noreply.github.com>
2021-12-07 01:49:27 +0800

    Finished Check Code function for codingPage

--- codeblockCar/templates/js/coding_page.js
+++ codeblockCar/templates/js/coding_page.js
@@ -193,61 +193,190 @@ $('#confirm-send').on('click', (e) => {
     });
 });
 
+// Static images
+const car = '<i class="bi bi-cart-fill"></i>';
+const arrow_up = '<i class="bi bi-arrow-up-circle-fill"></i>';
+const arrow_down = '<i class="bi bi-arrow-down-circle-fill"></i>';
+const arrow_left = '<i class="bi bi-arrow-left-circle-fill"></i>';
+const arrow_right = '<i class="bi bi-arrow-right-circle-fill"></i>';
+const crash = '<i class="bi bi-exclamation-square-fill"></i>'
 
-var lastClicked;
-createGrid();
+// Replace starting block with image of car
+document.getElementById('grid-1').innerHTML = car
 
-function clickableGrid(rows, cols) {
-    var i = 0;
-    var grid = document.createElement('table');
+var test_commands = '';
+var start_cell = 'grid-1'
 
-    grid.className = 'grid';
-    for (var r = 0; r < rows; ++r) {
-        var tr = grid.appendChild(document.createElement('tr'));
-        for (var c = 0; c < cols; ++c) {
-            var cell = tr.appendChild(document.createElement('td'));
-            if (i == 0) {
-                // cell.innerHTML = "Start"
-                cell.bgColor = 'green'
-                i++;
-                cell.id = i;
+// Direction 1 = up, 2 = right, 3 = down, 4 = left
+var direction = 2
+
+$('#test-code').on('click', (e) => {
+    if (getCode() == "data = ''\n") {
+        updateAlert("Error", "Nothing inside the workspace!");
+    }
+    else {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/coding/testcode",
+            headers: {'X-CSRFToken': csrftoken},
+            mode: 'same-origin',
+            data: {
+                // Get the data to be sent
+                "code": getCode(),
+                "log": false
+            },
+            error: function () {
+                // Show error message when unexpected errors occur
+                updateAlert("Error", "Error");
+            },
+            success: function (response) {
+                updateAlert("Success!", "You can now control the test!");
+                console.log('Data: ' + response)
+
+                test_commands = response;
+                
+                // Reveal hidden button
+                $("#run-command").removeClass("hidden");
+                document.getElementById("run-command").addEventListener("click", execute_test);
+
+                // setTimeout(function() {}, 1600);
             }
-            else if (i == (rows * cols) - 1) {
-                cell.innerHTML = "End"
-                cell.bgColor = 'red'
-                i++;
-                cell.id = i;
+        });
+    }
+});
+
+function calculate_cells(cell, cmd) {
+
+    // Get the current cell
+    current_cell = $('#'+cell).val()
+    var updated_cell_val = current_cell
+
+    console.log('Before update cell: ' + updated_cell_val)
+
+    // Logic to update the grid
+    if (cmd == 1) {
+        // Move upward
+        if (direction == 1) {
+            // Ensure that not a wall cell
+            if (current_cell => 1 && current_cell <= map_size) {
+                updated_cell_val = 0
             }
             else {
-                console.log(i, map[i])
-                if (map[i] == "1"){
-                    cell.className = "clicked"
-                    console.log("HIT" + i)
-                }
-                cell.innerHTML = ++i;
-                cell.id = i;
+                updated_cell_val = parseInt(current_cell) - parseInt(map_size)
+                // Move upward
+                document.getElementById(cell).innerHTML = arrow_up
+            }
+        }
+        // Move Right
+        else if (direction == 2) {
+            // Ensure that not a wall cell
+            if (current_cell % map_size == 0) {
+                updated_cell_val = 0
+            }
+            else {
+                updated_cell_val = parseInt(current_cell) + 1
+                // Move down
+                document.getElementById(cell).innerHTML = arrow_right
+            }
+        }
+        // Move Down
+        else if (direction == 3) {
+            // Ensure that not a wall cell
+            if (current_cell => (map_size * map_size - map_size + 1) && current_cell <= (map_size * map_size)) {
+                updated_cell_val = 0
+            }
+            else {
+                updated_cell_val = parseInt(current_cell) - parseInt(map_size)
+                // Move right
+                document.getElementById(cell).innerHTML = arrow_down
+            }
+        }
+        // Move Left
+        else if (direction == 4) {
+            // Ensure that not a wall cell
+            if ((current_cell + map_size - 1) % map_size == 0) {
+                updated_cell_val = 0
+            }
+            else {
+                updated_cell_val = parseInt(current_cell) - 1
+                // Move left
+                document.getElementById(cell).innerHTML = arrow_left
             }
         }
     }
-    return grid;
+    else if (cmd == 2) {
+        // Turn left
+        direction -= 1
+    }
+    else if (cmd == 3) {
+        // Turn right
+        direction += 1
+    }
+
+    console.log('Updated cell: ' + updated_cell_val)
+
+    // Return the updated cell id
+    return 'grid-' + updated_cell_val.toString()
 }
 
-function createGrid(e) {
-    var grid = clickableGrid(map_size, map_size, function (el, row, col, i){
-        console.log("You clicked on element:", el);
-        console.log("You clicked on row:", row);
-        console.log("You clicked on col:", col);
-        console.log("You clicked on item #:", i);
+function execute_test() {
+    // Set the first cell to be starting point
+    var prev_cell = start_cell
 
-        if ((row != 0 || col != 0) && (row != (3 - 1) || col != (3 - 1))) {
-            if (el.className == 'clicked'){
-                el.className = '';
-            }
-            else {
-                el.className = 'clicked';
-            }
+    // Ensure that there are commands to be run
+    for (let i = 0; i < test_commands.length; i++) {
+        // Get command based on position in string
+        cmd = test_commands.charAt(i)
+
+        console.log('Running command: ' + cmd.toString())
+
+        var next_cell = calculate_cells(prev_cell, cmd)
+
+        console.log('next cell: ' + next_cell)
+
+        // Ensure that not gone out of range
+        if (next_cell === 'grid-0') {
+            // Send alert message
+            updateAlert("Bang!", "Oops! Your car ran out of range. Please try again...");
+            document.getElementById(prev_cell).innerHTML = crash
+            // Hide the button again
+            $("#run-command").addClass("hidden");
+            return 1
         }
-    });
-    document.getElementById('TestGridSpace').appendChild(grid);
+        // Ensure that a wall is not hit
+        else if (next_cell.className === "clickable") {
+            // Send alert message
+            updateAlert("Bang!", "Oops! Your car bang the wall. Please try again...");
+            document.getElementById(prev_cell).innerHTML = crash
+            // Hide the button again
+            $("#run-command").addClass("hidden");
+            return 1
+        }
+        else {
+            document.getElementById(next_cell).innerHTML = car
+        }
+        
+        // Move the car position
+        prev_cell = next_cell
+
+        console.log('previous cell: ' + next_cell)
+        
+        // Sleep for 2s
+        setTimeout(() => {}, 2000);
+    }
+
+    if (prev_cell != 'grid-' + (map_size*map_size).toString()) {
+        // Send success message
+        updateAlert("Uh oh", "No more commands...");
+        // Hide the button again
+        $("#run-command").addClass("hidden");
+        return 0;
+    }
+    // Send success message
+    updateAlert("Yay!", "You did it!");
+    // Hide the button again
+    $("#run-command").addClass("hidden");
+    return 0;
 }
   
